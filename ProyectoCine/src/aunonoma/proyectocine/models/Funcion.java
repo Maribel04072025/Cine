8d5104830eaf8d961ce827db196b4ae01240c432
mateo <mateoq152006@gmail.com>
2025-04-05 21:46:36 -0500

    se cre la clase abtract de Funcion

---
+++ ProyectoCine/src/aunonoma/proyectocine/models/Funcion.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aunonoma.proyectocine.models;
+
+/**
+ *
+ * @author mateo
+ */
+public abstract class Funcion {
+    private String tipo;
+    public abstract double descuentoProsentaje();
+    
+}

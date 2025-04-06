ac53b46b599a207a34aa74eed34e0ad9a178ff2d
mateo <mateoq152006@gmail.com>
2025-04-05 22:44:22 -0500

    se cre el metodo de pelicula de tipo de funcion donde se va pedir qeu funcion le gustaria el usario

--- ProyectoCine/src/aunonoma/proyectocine/models/Pelicula.java
+++ ProyectoCine/src/aunonoma/proyectocine/models/Pelicula.java
@@ -4,6 +4,8 @@
  */
 package aunonoma.proyectocine.models;
 
+import java.util.Scanner;
+
 /**
  * Clase boleta donde se va dar toda la informacion de la pelicula
  * 
@@ -52,5 +54,44 @@ public class Pelicula {
         this.costoBase = costoBase;
     }
     
+    
+    public void tipoFuncion (){
+        
+        //se muestra lista de opciones
+       int op;
+       Scanner entrada = new Scanner(System.in);
+            System.out.println("\n=== Funciones para "+ this.titulo + " ===");
+            System.out.println("ingrese 1 para estar en la primer");
+            System.out.println("ingrese 2 para estar en la tarde");
+            System.out.println("ingrese 3 para estar en la noche");
+            
+            op = entrada.nextInt();
+            entrada.nextLine();
+            
+            switch (op){
+                case 1:
+                    
+                System.out.println("felicitaciones la primera funcion tendra descueto de 50%");
+                this.funcion = new PrimeraFuncion();
+                System.out.println("disfrute la pelicula");
+                break;   
+                case 2: 
+                System.out.println("felicitaciones la funcion de la tarde tendra descueto de 10%");
+                this.funcion = new FuncionTarde();
+                System.out.println("disfrute la pelicula");
+                break;
+                case 3:
+                System.out.println("lo lamentamos la funcion de la noche no tendra descueto");
+                this.funcion = new FuncionNoche();
+                System.out.println("disfrute la pelicula");
+                break;
+                
+                default:
+                System.out.println("numero incorrecto");
+                break;
+                
+    
+        }
+    }
     
 }

00c76e4f3c1295efbd5fd41ad1341c947a960f06
mateo <mateoq152006@gmail.com>
2025-04-05 21:20:14 -0500

    se cre la clase pelicula

---
+++ ProyectoCine/src/aunonoma/proyectocine/models/Pelicula.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aunonoma.proyectocine.models;
+
+/**
+ * Clase boleta donde se va dar toda la informacion de la pelicula
+ * 
+ * @author  Mateo Quintero
+ * @version 0.1
+ * @since   2025/07/20
+ * @see     autonoma.aunonoma.proyectocine.models
+ */
+public class Pelicula {
+    
+    
+}
